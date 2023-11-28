e589ba6ce2dd51b02c2bd625f439f9a37e4523e5
Joe-Klenam <klenamj3@gmail.com>
2023-11-28 06:12:00 +0000

    edited the createPost component

--- src/Components/ChatModal/ChatModal.jsx
+++ src/Components/ChatModal/ChatModal.jsx
@@ -2,28 +2,29 @@ import { IoClose } from "react-icons/io5";
 // styles
 import styles from "./ChatModal.module.css";
 
-const ChatModal = ({class_name, handleCloseChat}) => {
+const ChatModal = ({ class_name, handleCloseChat }) => {
 
-        
-const handleClose = (e)=>{
-        e.stopPropagation();
-       
-        handleCloseChat();
 
-}
+        const handleClose = (e) => {
+                console.log(e)
+                e.stopPropagation();
+
+                handleCloseChat();
+
+        }
         return (
-                <div 
-                onClick={(e)=>handleClose(e)}
-                className={class_name ? `${styles.chat_modal_container} ${styles.show_chat_modal_container}` : styles.chat_modal_container}>
+                <div
+                        onClick={(e) => handleClose(e)}
+                        className={class_name ? `${styles.chat_modal_container} ${styles.show_chat_modal_container}` : styles.chat_modal_container}>
 
                         <div className={styles.chat_modal}>
                                 <div className={styles.active_chat_top}>
                                         <div className={styles.active_receiver_details}>
                                                 <img src={""} alt="receiver_avatar" />
                                         </div>
-                                        <span 
-                                        onClick={handleCloseChat}
-                                         className={styles.cancel}>
+                                        <span
+                                                onClick={(e) => handleClose(e)}
+                                                className={styles.cancel}>
                                                 <IoClose />
                                         </span>
                                 </div>
